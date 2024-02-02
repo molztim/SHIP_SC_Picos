ee1d57b49168b3e84b37235328f1e5196bf7000e
molztim <92104590+molztim@users.noreply.github.com>
2024-02-02 14:47:15 +0100

    Delete dev directory

--- dev/ip_test.py
+++
@@ -1,5 +0,0 @@
-import ipaddress
-
-print(ipaddress.ip_network('192.168.0.0/24'))
-var = ipaddress.ip_address('192.168.0.1') in ipaddress.ip_network('192.168.0.0/24') #Basicly comapre if IP of device is in network IP
-print(var)
